db8a0a637e9d0d79e725fa1a04b6dc22c435921f
sitanshuk08 <sitanshuk@outlook.com>
2023-07-20 22:32:29 +0530

    developing dask version of multiple file read

--- directory_read.py
+++ directory_read.py
@@ -36,10 +36,51 @@ def directory_read(pathName, df):
     # print(count_file)
     df_1 = df.copy() 
     return df_1
+
+import os
+import pandas as pd
+import dask as dd
+
+
+def directory_read_dask(pathName, df):
+    # declare the variable global so they can be used outside function
+    global df_1
+    
+    # for loop which scans all the directories
+    for i in os.scandir(pathName):
+        # if the path ends in a file reads the csv or excel file else continue
+        if os.path.isfile(i):
+            
+            if i.path.endswith('.csv'):
+                df1 = dd.read_csv(i,low_memory = False)
+                
+            elif i.endswith('.xlsx'):
+                df1 = dd.read_excel(i)
+                
+            else: 
+                continue
+            df=dd.concat([df, df1], axis = 0)
+            
+        # if the director is subdirectory, calls the function to read the directory and appends to the existing dataframe
+        elif os.path.isdir(i):
+            # count_subdirectory = count_subdirectory+1
+            # print(count_subdirectory)
+            directory_read(i, df)
+        
+    # print(count_file)
+    df_1 = df.copy() 
+    return df_1
+
+
 # defining blank dataframe in which we will append the files being read
 df = pd.DataFrame()
+empty_dask_df = dd.from_delayed([df], meta=df)
+# empty_dask_df = dd.from_pandas(df, npartitions=1) 
+
 # defining the path variable
+# directory_read(path_test,df)
 path_test = r'/home/sitanshu/Documents/Python/datasets/tempreture/Files1'
-directory_read(path_test,df)
+
+directory_read_dask(path_test,df)
 df_1.head()
 
